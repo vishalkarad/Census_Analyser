0085b47875d5b6a180a7715f6990ab43a39bb2d6
vishalkarad <vishalkarad777@gmail.com>
2020-03-16 19:20:12 +0530

    Added : Check Number of records

--- src/main/java/CensusAnalyserMain.java
+++ src/main/java/CensusAnalyserMain.java
@@ -1,6 +1,27 @@
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+
+import javax.imageio.ImageReader;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class CensusAnalyserMain {
-    public static void main(String[] args) {
+
+    private static final String csvFilepath="StateCensusData.csv";
+    public static void main(String[] args)  {
         System.out.println("******************** Welcome to Census Analyseer ********************");
     }
-
+    // count number of records
+    public Integer readFile() throws Exception{
+        int count = 0;
+        Reader reader = Files.newBufferedReader(Paths.get(csvFilepath));
+        CSVReader csvReader = new CSVReader(reader);
+        String[] nextRecord;
+        while ((nextRecord = csvReader.readNext()) != null) {
+            count++;
+        }
+        return count;
+    }
 }
--- src/test/java/CensusAnalyserMainTest.java
+++ src/test/java/CensusAnalyserMainTest.java
@@ -1,2 +1,12 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CensusAnalyserMainTest {
+
+    CensusAnalyserMain censusAnalyser = new CensusAnalyserMain();
+    @Test
+    public void givenRecordInCSVFile_WhenRecordMatch_ThenTrue() throws Exception {
+        Integer record=censusAnalyser.readFile();
+        Assert.assertEquals((Integer) 30,record);
+    }
 }
