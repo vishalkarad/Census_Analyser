5081a94dd126ed445eb4c50ba08e947eb8ffbaf5
vishalkarad <vishalkarad777@gmail.com>
2020-03-16 12:35:46 +0530

    Added : Welcome to census analyser

---
+++ src/main/java/CensusAnalyserMain.java
@@ -0,0 +1,6 @@
+public class CensusAnalyserMain {
+    public static void main(String[] args) {
+        System.out.println("******************** Welcome to Census Analyseer ********************");
+    }
+
+}
