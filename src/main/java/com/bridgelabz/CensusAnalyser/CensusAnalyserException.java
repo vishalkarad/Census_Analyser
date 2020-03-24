b54b9b066b5228aa13760ad1a141e78d069b5767
vishalkarad <vishalkarad777@gmail.com>
2020-03-24 20:33:12 +0530

    Added : new exception. data are not fount

--- src/main/java/com/bridgelabz/CensusAnalyser/CensusAnalyserException.java
+++ src/main/java/com/bridgelabz/CensusAnalyser/CensusAnalyserException.java
@@ -3,7 +3,7 @@ package com.bridgelabz.CensusAnalyser;
 public class CensusAnalyserException extends Exception {
 
     public enum MyException_Type{
-        FILE_NOT_FOUND,DELIMITER_INCORECT;
+        FILE_NOT_FOUND,DELIMITER_INCORECT,DATA_ARE_NOT_FOUND;
     }
     public MyException_Type type;
     public CensusAnalyserException(MyException_Type type, String message) {
