bc9663a8c37a0c880178a1be4a0e1fd0d10b5f53
vishalkarad <vishalkarad777@gmail.com>
2020-03-28 05:34:59 +0530

    Added : geter methods it

--- src/main/java/com/bridgelabz/CensusAnalyser/IndianStateCensusData.java
+++ src/main/java/com/bridgelabz/CensusAnalyser/IndianStateCensusData.java
@@ -15,4 +15,19 @@ public class IndianStateCensusData {
         @CsvBindByName(column = "DensityPerSqKm",required = true)
         public int densityPerSqKm;
 
+        public String getState() {
+                return state;
+        }
+
+        public int getPopulation() {
+                return population;
+        }
+
+        public int getAreaInSqKm() {
+                return areaInSqKm;
+        }
+
+        public int getDensityPerSqKm() {
+                return densityPerSqKm;
+        }
 }
---
+++ src/main/java/com/bridgelabz/CensusDAO/CensusAnalyserDAO.java
@@ -0,0 +1,4 @@
+package com.bridgelabz.CensusDAO;
+
+public class CensusAnalyserDAO {
+}
