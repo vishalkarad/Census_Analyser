d1f3163bf1b11ee431b3a1f8040610a2902db333
vishalkarad <vishalkarad777@gmail.com>
2020-03-28 05:37:40 +0530

    Added : Create New Census DAO Class

--- src/main/java/com/bridgelabz/CensusDAO/CensusAnalyserDAO.java
+++ src/main/java/com/bridgelabz/CensusDAO/CensusAnalyserDAO.java
@@ -1,4 +1,22 @@
 package com.bridgelabz.CensusDAO;
+import com.bridgelabz.CensusAnalyser.IndianStateCensusData;
+import com.bridgelabz.CensusAnalyser.StateCodePOJO;
 
 public class CensusAnalyserDAO {
+    public String state;
+    public int population;
+    public int area;
+    public int density;
+    public String stateCode;
+
+    public CensusAnalyserDAO(IndianStateCensusData csvStateCensus) {
+        this.state = csvStateCensus.getState();
+        this.population = csvStateCensus.getPopulation();
+        this.area = csvStateCensus.getAreaInSqKm();
+        this.density = csvStateCensus.getDensityPerSqKm();
+    }
+
+    public CensusAnalyserDAO(StateCodePOJO csvStateCode) {
+        this.stateCode = csvStateCode.getStateCode();
+    }
 }
