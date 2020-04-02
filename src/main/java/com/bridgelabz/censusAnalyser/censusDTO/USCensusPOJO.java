2ebd912f5973d6ee1c92f1d66654c340b2a0d0f6
vishalkarad <vishalkarad777@gmail.com>
2020-04-03 01:05:05 +0530

    Added : Parameterised constructor

--- src/main/java/com/bridgelabz/censusAnalyser/censusDTO/USCensusPOJO.java
+++ src/main/java/com/bridgelabz/censusAnalyser/censusDTO/USCensusPOJO.java
@@ -22,6 +22,14 @@ public class USCensusPOJO {
     @CsvBindByName(column = "Housing Density",required = true)
     public float housing_Density;
 
+    public USCensusPOJO(String state_Id, String state, int population, int area, int population1) {
+        this.state_Id = state_Id;
+        this.state = state;
+        this.population = population;
+        this.total_Area = area;
+        this.population = population;
+    }
+
     public String getState_Id() {
         return state_Id;
     }
